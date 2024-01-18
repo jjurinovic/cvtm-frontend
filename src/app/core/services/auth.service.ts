0a535349f1a8536c734271a7a6ec773bb4a7d36e
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-18 21:19:41 +0100

    added auth login method to auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,12 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  BASE_URL = 'http://localhost:8000/';
 
   isLoggedIn(): boolean {
     return true;
+  }
+
+  login(username: string, password: string): Observable<any> {
+    const formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', password);
+    return this.http.post(this.BASE_URL + 'login', formData);
   }
 }
