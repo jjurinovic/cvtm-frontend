40a5efb8e33baafa9adcbef87a15bca5d51373de
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-18 22:18:43 +0100

    added simple logout to base component

--- src/app/core/layout/base/base.component.ts
+++ src/app/core/layout/base/base.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { logout } from '../../../state/auth/auth.actions';
 
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
   styleUrl: './base.component.scss',
 })
-export class BaseComponent {}
+export class BaseComponent {
+  constructor(private _auth: AuthService) {}
+
+  logout(): void {
+    this._auth.logout();
+  }
+}
