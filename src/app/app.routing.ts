f26bd9285cadeb677f179cff2ec15ae44dc0e717
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-18 02:52:23 +0100

    added redirect path and auth guard to home path

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/auth/login/login.component';
 import { BaseComponent } from './core/layout/base/base.component';
+import { authGuard } from './guards/auth-guard.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: BaseComponent },
+  { path: '', component: BaseComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '/' },
 ];
 
 @NgModule({
