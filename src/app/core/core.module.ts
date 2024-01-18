1f3914cb9437c88daf4cd763b040f0e29bf4f9d0
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-18 02:51:47 +0100

    make that login component is not standalone

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BaseComponent } from './layout/base/base.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
-  declarations: [BaseComponent, HeaderComponent, SidebarComponent],
+  declarations: [
+    BaseComponent,
+    HeaderComponent,
+    SidebarComponent,
+    LoginComponent,
+  ],
   imports: [CommonModule],
 })
 export class CoreModule {}
