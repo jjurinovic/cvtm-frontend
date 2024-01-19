5a0872e34efbed368ea6c623e4300f5e73413953
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-19 01:10:19 +0100

    use loading for login

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as AuthActions from '../../../state/auth/auth.actions';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { selectIsLoading } from 'src/app/state/auth/auth.selectors';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
   public form: FormGroup;
   public hide: boolean = true;
+  public isLoading: boolean = false;
 
   constructor(private store: Store, private fb: FormBuilder) {
     this.form = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
+
+    this.store
+      .select(selectIsLoading)
+      .subscribe((val: boolean) => (this.isLoading = val));
   }
 
   public submit(): void {
