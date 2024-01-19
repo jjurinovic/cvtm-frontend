4eac463f07fa4abb3f874d0a5f89fee7d28d06ac
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-19 20:54:30 +0100

    implemented navigation tests

--- src/app/core/layout/navigation/navigation.component.spec.ts
+++ src/app/core/layout/navigation/navigation.component.spec.ts
@@ -3,6 +3,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavigationComponent } from './navigation.component';
 import { MatListModule } from '@angular/material/list';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { By } from '@angular/platform-browser';
+
+const testLinks = [
+  { link: 'test1', title: 'Test 1' },
+  { link: 'test2', title: 'Test 2' },
+  { link: 'test3', title: 'Test 3' },
+];
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
@@ -21,5 +28,19 @@ describe('NavigationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render mat-nav-list', () => {
+    const matNav = fixture.debugElement.query(By.css('mat-nav-list'));
+    expect(matNav).toBeTruthy();
+  });
+
+  it('should render 3 navigation items', () => {
+    component.links = testLinks;
+    fixture.detectChanges();
+
+    const items = fixture.debugElement.queryAll(By.css('.navigation-item'));
+
+    expect(items.length).toEqual(3);
   });
 });
