7fa9d5593b5cd1cfb3b4b5266cce5bd92b0ac97e
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-19 01:18:47 +0100

    added toolbar to header and moved logout from base to header

--- src/app/core/layout/base/base.component.ts
+++ src/app/core/layout/base/base.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { logout } from '../../../state/auth/auth.actions';
 
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
   styleUrl: './base.component.scss',
 })
-export class BaseComponent {
-  constructor(private _auth: AuthService) {}
-
-  logout(): void {
-    this._auth.logout();
-  }
-}
+export class BaseComponent {}
--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private _auth: AuthService) {}
+
+  logout(): void {
+    this._auth.logout();
+  }
+}
