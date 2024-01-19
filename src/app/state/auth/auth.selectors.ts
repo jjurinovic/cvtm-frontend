bb9ba1afffdfdd2df791d2ac9688bedb0b00da85
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-19 01:09:12 +0100

    added auth selectors

---
+++ src/app/state/auth/auth.selectors.ts
@@ -0,0 +1,15 @@
+import { createSelector } from '@ngrx/store';
+
+const selectAuth = (state: any) => state.auth;
+
+export const selectIsLoggedIn = createSelector(
+  selectAuth,
+  (state) => state.isLoggedIn
+);
+
+export const selectError = createSelector(selectAuth, (state) => state.error);
+
+export const selectIsLoading = createSelector(
+  selectAuth,
+  (state) => state.isLoading
+);
