7566deae8bda7c47ebd07248bc101326cf28be7f
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-19 14:35:38 +0100

    added header to show current user name

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from 'src/app/shared/components/confirm-dialog/confirm-dialog.component';
+import { Store } from '@ngrx/store';
+import { selectCurrentUser } from 'src/app/state/auth/auth.selectors';
+import { User } from 'src/app/features/users/models/user.model';
 
 @Component({
   selector: 'app-header',
@@ -10,8 +13,17 @@ import { ConfirmDialogComponent } from 'src/app/shared/components/confirm-dialog
 })
 export class HeaderComponent {
   @Output() onMenuClick: EventEmitter<any> = new EventEmitter();
+  currentUser?: User;
 
-  constructor(private _auth: AuthService, public dialog: MatDialog) {}
+  constructor(
+    private _auth: AuthService,
+    public dialog: MatDialog,
+    private store: Store
+  ) {
+    this.store
+      .select(selectCurrentUser)
+      .subscribe((user) => (this.currentUser = user));
+  }
 
   logout(): void {
     this._auth.logout();
