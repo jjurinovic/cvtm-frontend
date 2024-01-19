0d3d9834c22be87a59ca1f8c47a1e5d84d469c6c
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-19 20:38:15 +0100

    header components fixed

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from 'src/app/shared/components/confirm-dialog/confirm-dialog.component';
 import { Store } from '@ngrx/store';
@@ -16,11 +15,7 @@ export class HeaderComponent {
   @Output() onMenuClick: EventEmitter<any> = new EventEmitter();
   currentUser?: User;
 
-  constructor(
-    private _auth: AuthService,
-    public dialog: MatDialog,
-    private store: Store
-  ) {
+  constructor(public dialog: MatDialog, private store: Store) {
     this.store
       .select(selectCurrentUser)
       .subscribe((user) => (this.currentUser = user));
