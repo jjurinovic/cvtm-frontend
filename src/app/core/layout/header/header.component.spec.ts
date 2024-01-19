713a81ad69933279b61d4b3ef36f26126e47ef60
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-19 20:38:24 +0100

    header component tests

--- src/app/core/layout/header/header.component.spec.ts
+++ src/app/core/layout/header/header.component.spec.ts
@@ -5,6 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { selectCurrentUser } from 'src/app/state/auth/auth.selectors';
+import { User } from 'src/app/features/users/models/user.model';
+import { Role } from 'src/app/features/users/enums/role.enum';
+import { By } from '@angular/platform-browser';
+import { logout } from 'src/app/state/auth/auth.actions';
+
+const testUser: User = {
+  id: 2,
+  name: 'test name',
+  email: 'abc@test.com',
+  role: Role.ADMIN,
+  company_id: 1,
+};
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -22,10 +35,22 @@ describe('HeaderComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
       imports: [HttpClientModule, MatToolbarModule, MatIconModule],
-      providers: [provideMockStore({ initialState })],
+      providers: [
+        provideMockStore({
+          initialState,
+          selectors: [
+            {
+              selector: selectCurrentUser,
+              value: testUser,
+            },
+          ],
+        }),
+      ],
     }).compileComponents();
 
     store = TestBed.inject(MockStore);
+    spyOn(store, 'select').and.callThrough();
+    spyOn(store, 'dispatch').and.callThrough();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -34,5 +59,25 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get current user from store', async () => {
+    expect(store.select).toHaveBeenCalledWith(selectCurrentUser);
+    expect(component.currentUser).toEqual(testUser);
+  });
+
+  it('should open confirm dialog', () => {
+    const openDialogSpy = spyOn(component, 'openDialog');
+    const logoutBtn = fixture.debugElement.query(By.css('.header-logout'));
+    logoutBtn.nativeElement.click();
+
+    fixture.detectChanges();
+    expect(openDialogSpy).toHaveBeenCalled();
+  });
+
+  it('should call logout', () => {
+    component.logout();
+
+    expect(store.dispatch).toHaveBeenCalledWith(logout());
   });
 });
