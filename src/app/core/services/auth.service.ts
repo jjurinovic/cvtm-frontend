6650ce938b70bb333b4b0b6d6c0e47b09c897b8d
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-20 02:45:35 +0100

    added setToken() method to auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -29,6 +29,10 @@ export class AuthService {
     return localStorage.getItem('token');
   }
 
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
   removeToken(): void {
     localStorage.removeItem('token');
   }
