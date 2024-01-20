557a113f5f6ecea7ad02e5dae08a7d0f76c2d8cc
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-20 01:31:37 +0100

    implemented tests for auth interceptor

---
+++ src/app/interceptors/auth.incerceptor.spec.ts
@@ -0,0 +1,81 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { UsersService } from '../features/users/services/users.service';
+import { User } from '../features/users/models/user.model';
+import { Role } from '../features/users/enums/role.enum';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+
+const testToken = 'abc';
+const testUser: User = {
+  id: 2,
+  name: 'test name',
+  email: 'abc@test.com',
+  role: Role.ADMIN,
+  company_id: 1,
+};
+
+describe('AuthInterceptor', () => {
+  let service: UsersService;
+  let httpTestingController: HttpTestingController;
+
+  let url = 'http://localhost:8000';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthInterceptor,
+          multi: true,
+        },
+      ],
+    });
+    service = TestBed.inject(UsersService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should intercept request and add token if token exists', () => {
+    localStorage.setItem('token', testToken);
+    service.getCurrentUser().subscribe((res) => {
+      expect(res).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne({
+      method: 'GET',
+      url: `${url}/user/me`,
+    });
+    expect(req.request.headers.has('Content-Type')).toEqual(true);
+    expect(req.request.headers.get('Content-Type')).toEqual('application/json');
+    expect(req.request.headers.has('Authorization')).toEqual(true);
+    expect(req.request.headers.get('Authorization')).toEqual(
+      `Bearer ${testToken}`
+    );
+  });
+
+  it('should intercept request and not add token if token does not exist', () => {
+    localStorage.removeItem('token');
+    service.getCurrentUser().subscribe((res) => {
+      expect(res).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne({
+      method: 'GET',
+      url: `${url}/user/me`,
+    });
+    expect(req.request.headers.has('Content-Type')).toEqual(false);
+    expect(req.request.headers.has('Authorization')).toEqual(false);
+  });
+});
