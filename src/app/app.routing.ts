e7bf182196dbea6b2e902ef9b19b6d2adeefb9e3
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-22 00:44:47 +0100

    added admin to routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/auth/login/login.component';
 import { BaseComponent } from './core/layout/base/base.component';
 import { authGuard } from './guards/auth.guard';
+import { roleGuard } from './guards/role.guard';
+import { Role } from './features/users/enums/role.enum';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -18,6 +20,14 @@ const routes: Routes = [
           import('./features/dashboard/dashboard.module').then(
             (m) => m.DashboardModule
           ),
+      },
+      {
+        path: 'admin',
+        canActivate: [roleGuard, authGuard],
+        canActivateChild: [roleGuard],
+        data: { role: Role.ROOT },
+        loadChildren: () =>
+          import('./features/admin/admin.module').then((m) => m.AdminModule),
       },
       {
         path: 'company',
