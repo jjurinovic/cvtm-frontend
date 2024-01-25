80c1e192632d4954ab624e52debaed2c94878234
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-25 02:54:32 +0100

    added tabs to admin component

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './components/admin/admin.component';
 import { AdminRoutingModule } from './admin.routing';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [CommonModule, AdminRoutingModule, MatTabsModule],
 })
 export class AdminModule {}
