0a41f11ef00bac2596daafdcf8b3914bd0f53b43
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-25 03:49:36 +0100

    added tabs to admin component, and table with all users

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './components/admin/admin.component';
 import { AdminRoutingModule } from './admin.routing';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [CommonModule, AdminRoutingModule, MatTabsModule],
+  imports: [CommonModule, AdminRoutingModule, MatTabsModule, MatTableModule],
 })
 export class AdminModule {}
--- src/app/features/admin/components/admin/admin.component.ts
+++ src/app/features/admin/components/admin/admin.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as CompanyActions from '../../../company/state/company.actions';
+import { selectAllCompanies } from 'src/app/features/company/state/company.selectors';
+import { Company } from 'src/app/features/company/models/company.model';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+  styleUrl: './admin.component.scss',
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'vat', 'address'];
+  companies: Company[] = [];
+  constructor(private store: Store) {}
 
+  ngOnInit(): void {
+    this.store.dispatch(CompanyActions.getAll());
+
+    this.store.select(selectAllCompanies).subscribe((data) => {
+      this.companies = data;
+      console.log(this.companies);
+    });
+  }
 }
