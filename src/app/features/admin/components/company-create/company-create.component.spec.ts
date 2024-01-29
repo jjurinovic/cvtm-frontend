c760bd35cbc152723945ed36736d81666dc3cdab
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-29 03:43:48 +0100

    fixed admin create company test to show no errors

--- src/app/features/admin/components/company-create/company-create.component.spec.ts
+++ src/app/features/admin/components/company-create/company-create.component.spec.ts
@@ -1,17 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 import { CompanyCreateComponent } from './company-create.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CompanyCreateComponent', () => {
   let component: CompanyCreateComponent;
   let fixture: ComponentFixture<CompanyCreateComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CompanyCreateComponent]
-    })
-    .compileComponents();
-    
+      declarations: [CompanyCreateComponent],
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatCardModule,
+      ],
+      providers: [provideMockStore()],
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(CompanyCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
