42bde37134334d60b9bd70962bfc0a567efe3a5a
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-02 22:18:19 +0100

    core module import organize

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BaseComponent } from './layout/base/base.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { MainComponent } from './layout/main/main.component';
-import { LoginComponent } from './auth/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, TitleStrategy } from '@angular/router';
+
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterModule, TitleStrategy } from '@angular/router';
-import { NavigationComponent } from './layout/navigation/navigation.component';
 import { MatListModule } from '@angular/material/list';
+
+import { BaseComponent } from './layout/base/base.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from './layout/main/main.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 import { HasRoleDirective } from '../shared/directives/has-role.directive';
 import { PageTitleComponent } from './layout/page-title/page-title.component';
 import { PageTitleStrategy } from './strategies/page-title-strategy';
