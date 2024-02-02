01be641899ec0fe7c363aa6848f5ba935b70bf38
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-02 03:13:01 +0100

    added user admin routes and to admin users tab

--- src/app/features/admin/admin.routing.ts
+++ src/app/features/admin/admin.routing.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AdminComponent } from './components/admin/admin.component';
 import { CompanyFormComponent } from './components/company-form/company-form.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,16 @@ const routes: Routes = [
     path: 'company/create',
     component: CompanyFormComponent,
     title: 'Create Company',
+  },
+  {
+    path: 'user/:id/edit',
+    component: UserFormComponent,
+    title: 'Edit User',
+  },
+  {
+    path: 'user/create',
+    component: UserFormComponent,
+    title: 'Create User',
   },
 ];
 
