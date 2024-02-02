9d4cb1d0b32e32597baa9c0b7d088d8e50ff5804
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-02 03:06:24 +0100

    added methods for create user, getUserById and get all users

--- src/app/features/users/services/users.service.ts
+++ src/app/features/users/services/users.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
 import { environment } from 'src/environments/environment';
+import { PageResponse } from 'src/app/shared/models/page-response';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,32 @@ export class UsersService {
    */
   public getCurrentUser(): Observable<User> {
     return this.http.get<User>(this.baseUrl + '/me');
+  }
+
+  /**
+   * Create new user and return it
+   * @param {User} user user data for create
+   * @returns {Observable<User>} Returns Observable with created user
+   */
+  public createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.baseUrl, user);
+  }
+
+  /**
+   * Return user by given id
+   * @param {number} id
+   * @returns {Observable<User>} Returns Observable with user with given id
+   */
+  public getUserById(id: number): Observable<User> {
+    return this.http.get<User>(this.baseUrl + `/${id}`);
+  }
+
+  /**
+   * Returns all user
+   * @param {HttpParams} params pagination, search
+   * @returns {Observable<PageResponse<User>>} Return Observable with page response with list of users
+   */
+  public getAll(params: HttpParams): Observable<PageResponse<User>> {
+    return this.http.get<PageResponse<User>>(this.baseUrl, { params });
   }
 }
