6bb079a9585c335206ec9bc4d5e1f7fe5360edff
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-05 19:42:46 +0100

    removed unused component

--- src/app/features/company/company.module.ts
+++ src/app/features/company/company.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CompanyEditComponent } from './components/company-edit/company-edit.component';
-import { CompanyListComponent } from './components/company-list/company-list.component';
-import { CompanyInfoComponent } from './components/company-info/company-info.component';
 import { CompanyRoutingModule } from './company.routing';
-import { CompanyComponent } from './components/company/company.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { CompanyEditComponent } from './components/company-edit/company-edit.component';
+import { CompanyInfoComponent } from './components/company-info/company-info.component';
+import { CompanyComponent } from './components/company/company.component';
+
 @NgModule({
-  declarations: [
-    CompanyEditComponent,
-    CompanyListComponent,
-    CompanyInfoComponent,
-    CompanyComponent,
-  ],
+  declarations: [CompanyEditComponent, CompanyInfoComponent, CompanyComponent],
   imports: [CommonModule, CompanyRoutingModule, HttpClientModule],
 })
 export class CompanyModule {}
