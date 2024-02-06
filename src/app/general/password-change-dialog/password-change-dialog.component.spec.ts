cd40a18dac8c94e3142803f9215a930f64f03224
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-06 03:02:48 +0100

    fixed broken unit tests

--- src/app/general/password-change-dialog/password-change-dialog.component.spec.ts
+++ src/app/general/password-change-dialog/password-change-dialog.component.spec.ts
@@ -1,18 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { PasswordChangeDialogComponent } from './password-change-dialog.component';
 
 describe('PasswordChangeDialogComponent', () => {
   let component: PasswordChangeDialogComponent;
   let fixture: ComponentFixture<PasswordChangeDialogComponent>;
+  let dialog: MatDialogRef<PasswordChangeDialogComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PasswordChangeDialogComponent]
-    })
-    .compileComponents();
-    
+      declarations: [PasswordChangeDialogComponent],
+      imports: [
+        MatDialogModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [{ provide: MatDialogRef, useValue: {} }, provideMockStore()],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PasswordChangeDialogComponent);
+    dialog = TestBed.inject(MatDialogRef);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/general/profile/profile.component.spec.ts
+++ src/app/general/profile/profile.component.spec.ts
@@ -1,19 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 import { ProfileComponent } from './profile.component';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProfileComponent]
-    })
-    .compileComponents();
-    
+      declarations: [ProfileComponent],
+      providers: [provideMockStore()],
+      imports: [MatCardModule, MatInputModule, ReactiveFormsModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
