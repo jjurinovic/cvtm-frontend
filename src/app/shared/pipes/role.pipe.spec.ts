e0ffab09c245328a281b7f2cde70d2f4ef96a9db
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-06 03:12:00 +0100

    implemented tests for role pipe

--- src/app/shared/pipes/role.pipe.spec.ts
+++ src/app/shared/pipes/role.pipe.spec.ts
@@ -1,8 +1,26 @@
+import { Role } from 'src/app/features/users/enums/role.enum';
 import { RolePipe } from './role.pipe';
 
 describe('RolePipe', () => {
+  const pipe = new RolePipe();
+
   it('create an instance', () => {
-    const pipe = new RolePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return ROOT for value of Role.ROOT', () => {
+    expect(pipe.transform(Role.ROOT)).toEqual('ROOT');
+  });
+
+  it('should return ADMIN for value Role.ADMIN', () => {
+    expect(pipe.transform(Role.ADMIN)).toEqual('ADMIN');
+  });
+
+  it('should return MODERATOR for value Role.MODERATOR', () => {
+    expect(pipe.transform(Role.MODERATOR)).toEqual('MODERATOR');
+  });
+
+  it('should return USER for value Role.USER', () => {
+    expect(pipe.transform(Role.USER)).toEqual('USER');
   });
 });
