6836004e853b285b84a2333e99dc8140b72a9c71
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-14 01:48:55 +0100

    removed unused import from time tracking service

--- src/app/features/time-tracking/services/time-tracking.service.ts
+++ src/app/features/time-tracking/services/time-tracking.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
